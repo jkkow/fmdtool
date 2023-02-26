b87f5f122a926b997ed6b8359b60ea824d6fe5fb
jkkow <jkwonko@gmail.com>
2023-02-26 22:49:42 +0900

    Add some test

--- mode_analysis/usolver.py
+++ mode_analysis/usolver.py
@@ -5,9 +5,10 @@ from scipy.optimize import fsolve
 
 class WGFiber:
 
+    def __init__(self, v=None):
+        if v is not None:
+            self.v = v
 
-    def __init__(self, v):
-        self.v = v
 
     def gen_eigen_eq(self, l):
         v = self.v
@@ -24,27 +25,51 @@ class WGFiber:
         
 
     def find_max_jn_zeros(self, l):
-        start = 1
-        while jn_zeros(l, start)[-1] < self.v:
-            start += 1
-        max_num = start
-        return max_num-1
+        lo = 1
+        while jn_zeros(l, lo)[-1] < self.v:
+            lo += 1
+            if lo > 100:
+                raise ValueError("Too many points of Bessel zeros")
+        maxn = lo
+        return maxn-1
 
 
-    def get_diverging_points(self, l):
-        maxnum = self.find_max_jn_zeros(l)
-        return jn_zeros(l, maxnum)
+    def get_init_points_to_solve(self, l):
+        v = self.v
+        offset1 = 0.000001 # Don't change this value
+        offset2 = 0.5
+        lhs_eq_at_v = self.left_side_eigen_eq(l)(v)
+        maxn = self.find_max_jn_zeros(l)
+        if maxn == 0 :
+            if lhs_eq_at_v > 0:
+                return None
+            else:
+                init_points = np.array(v-offset1)
+                return init_points
+        else:
+            init_points = jn_zeros(l, maxn) - offset2
+            if lhs_eq_at_v > 0:
+                return init_points
+            else:
+                return np.append(init_points, v-offset1)
+
+
+    def get_roots_for_u(self, l):
+        init_points = self.get_init_points_to_solve(l)
+        try:
+            roots = fsolve(self.gen_eigen_eq(l), init_points)
+            return roots
+        except RuntimeError as e:
+            print(e)
+
         
 
-    def usolve(self, l):
-        eigen_eq = self.gen_eigen_eq(l)
-        initp = self.get_init_points_to_solve(l)
-        roots = fsolve(eigen_eq, initp)
-        dict_u = {}
-        for m in len(roots):
-            dict_u[f"ul{m}"] = roots[m]
-        return dict_u
 
-            
+
 if __name__ == "__main__":
-    wgf = WGFiber(5)
+    wgf = WGFiber(7.02)
+    print(wgf.left_side_eigen_eq(l=0)(wgf.v))
+    print(wgf.get_init_points_to_solve(l=0))
+    print(wgf.get_roots_for_u(l=0))
+    print(type(wgf.get_roots_for_u(l=0)))
+
--- tests/test_usolver.py
+++ tests/test_usolver.py
@@ -1,10 +1,7 @@
-import pytest
+import numpy as np
 from scipy.special import jn_zeros
 from mode_analysis.usolver import WGFiber
 
-
-def test_gen_eigen_eq():
-    wgf = WGFiber(3)
 
 
 def test_find_max_jn_zeros():
@@ -22,11 +19,30 @@ def test_find_max_jn_zeros():
     assert wgf.find_max_jn_zeros(l=2) == 1
 
     
+def test_get_init_points_to_solve():
+    wgf = WGFiber(v=2.0)
+    assert wgf.get_init_points_to_solve(l=0) != None
+    assert np.size(wgf.get_init_points_to_solve(l=0)) == 1
+    assert np.size(wgf.get_init_points_to_solve(l=1)) == 1 # 'None' is also counted.
+    assert wgf.get_init_points_to_solve(l=1) == None
     
-
-
-
+    wgf = WGFiber(v=2.406)
+    assert wgf.get_init_points_to_solve(l=0) != None
+    assert wgf.get_init_points_to_solve(l=1) != None
+    assert wgf.get_init_points_to_solve(l=2) == None
     
+    wgf = WGFiber(v=7.0)
+    assert np.size(wgf.get_init_points_to_solve(l=0)) == 2
+    assert np.size(wgf.get_init_points_to_solve(l=1)) == 2
+    assert np.size(wgf.get_init_points_to_solve(l=2)) == 1
+    assert np.size(wgf.get_init_points_to_solve(l=3)) == 1
+    assert np.size(wgf.get_init_points_to_solve(l=4)) == 1
+    assert wgf.get_init_points_to_solve(l=5) == None
 
-
-
+    wgf = WGFiber(v=7.02)
+    assert np.size(wgf.get_init_points_to_solve(l=0)) == 3
+    assert np.size(wgf.get_init_points_to_solve(l=1)) == 2
+    assert np.size(wgf.get_init_points_to_solve(l=2)) == 2
+    assert np.size(wgf.get_init_points_to_solve(l=3)) == 1
+    assert np.size(wgf.get_init_points_to_solve(l=4)) == 1
+    assert wgf.get_init_points_to_solve(l=5) == None
