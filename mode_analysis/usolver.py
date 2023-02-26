8095939c1905d3af8040fa6e2e4aa38b88466d1b
jkkow <jkwonko@gmail.com>
2023-02-26 14:02:48 +0900

    Add some test

--- mode_analysis/usolver.py
+++ mode_analysis/usolver.py
@@ -1,15 +1,50 @@
 import numpy as np
-from scipy.special import jv, kv
+from scipy.special import jv, kv, jn_zeros
+from scipy.optimize import fsolve
 
 
 class WGFiber:
 
-    def __init__(self, V=None):
-        if V is not None:
-            self.V = V
 
-    def gen_eigen_eq(self, V, l):
+    def __init__(self, v):
+        self.v = v
+
+    def gen_eigen_eq(self, l):
+        v = self.v
         def wrapper(u):
-            w = np.sqrt(V*V - u*u)
-            return jv(l - 1, u) / jv(l, u) + (w / u) * kv(l - 1, w) / kv(l, w)
+            w = np.sqrt(v*v - u*u)
+            return u*jv(l-1, u)/jv(l, u) + w*kv(l-1, w)/kv(l, w)
         return wrapper
+
+
+    def left_side_eigen_eq(self, l):
+        def wrapper(u):
+            return u*jv(l-1, u)/jv(l, u)
+        return wrapper
+        
+
+    def find_max_jn_zeros(self, l):
+        start = 1
+        while jn_zeros(l, start)[-1] < self.v:
+            start += 1
+        max_num = start
+        return max_num-1
+
+
+    def get_diverging_points(self, l):
+        maxnum = self.find_max_jn_zeros(l)
+        return jn_zeros(l, maxnum)
+        
+
+    def usolve(self, l):
+        eigen_eq = self.gen_eigen_eq(l)
+        initp = self.get_init_points_to_solve(l)
+        roots = fsolve(eigen_eq, initp)
+        dict_u = {}
+        for m in len(roots):
+            dict_u[f"ul{m}"] = roots[m]
+        return dict_u
+
+            
+if __name__ == "__main__":
+    wgf = WGFiber(5)
--- tests/test_usolver.py
+++ tests/test_usolver.py
@@ -1,12 +1,32 @@
 import pytest
+from scipy.special import jn_zeros
 from mode_analysis.usolver import WGFiber
 
 
-def test_make_instance():
-    wgf = WGFiber()
-    wgf.V = 10
-    assert wgf.V == 10
+def test_gen_eigen_eq():
+    wgf = WGFiber(3)
+
+
+def test_find_max_jn_zeros():
+    wgf = WGFiber(v=2.0)
+    assert wgf.find_max_jn_zeros(l=0) == 0
+    assert wgf.find_max_jn_zeros(l=1) == 0
+ 
+    wgf = WGFiber(v=2.6)
+    assert wgf.find_max_jn_zeros(l=0) == 1
+    assert wgf.find_max_jn_zeros(l=1) == 0
+    
+    wgf = WGFiber(v=5.5)
+    assert wgf.find_max_jn_zeros(l=0) == 1
+    assert wgf.find_max_jn_zeros(l=1) == 1
+    assert wgf.find_max_jn_zeros(l=2) == 1
+
+    
     
 
-if __name__ == "__main__":
-    wgf = WGFiber()
+
+
+    
+
+
+
