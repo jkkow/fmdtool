a27bd55d9c5d1ce1d84240a1c9cf94a25f664cbb
jkkow <jkwonko@gmail.com>
2023-03-05 22:12:50 +0900

    Add function 'LP' in mtool

--- mode_analysis/mtool.py
+++ mode_analysis/mtool.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from .usolver import WGFiber 
+from usolver import WGFiber 
 import numpy as np
 from scipy.special import jv, kv
 
@@ -10,6 +10,7 @@ class LPModes(WGFiber):
         super().__init__(v)
         self.V = v  # V number
         self.a = dia_core/2 # radius of core in micron 
+        self.mesh_size = 800
 
 
     def field_core_eq(self, l):
@@ -23,31 +24,36 @@ class LPModes(WGFiber):
         v = self.V
         a = self.a
         def wrapper(u, r, phi):
-            w = np.sqrt(v*v - a*a)
+            w = np.sqrt(v*v - u*u)
             return (jv(l, u)/kv(l, w))*kv(l, w*r/a) * np.cos(l*phi)
         return wrapper
 
 
-    def is_core(self, x, y):
+    def mask_core(self, x, y):
         return np.sqrt(x*x+y*y) <= self.a
 
 
-    def is_cald(self, x, y):
+    def mask_clad(self, x, y):
         return np.sqrt(x*x+y*y) > self.a
 
 
-    def LP(self, l, m, meshsize=800):
+    def LP(self, l, m):
         a = self.a
 
-        xi = np.linspace(-1.5*a, 1.5*a, meshsize)
+        xi = np.linspace(-1.5*a, 1.5*a, self.mesh_size)
         x, y = np.meshgrid(xi, xi, indexing='xy')
 
         u = self.u_set.get(f'u{l}{m}')
         r = np.sqrt(x*x + y*y)
         phi = np.arctan2(x, y)
+
         fcore = self.field_core_eq(l)
-        fclad = self.filed_clad_eq(l)
-        e_field_core = fcore(u, r, phi)*is_core(x,y)
-        e_field_clad = fclad(u, r, phi)*is_clad(x,y)
-        mode_filed = e_feild_core + e_field+clad
+        mcore = self.mask_core(x,y)
+        e_field_core = fcore(u, r, phi)*mcore
+
+        fclad = self.field_clad_eq(l)
+        mclad = self.mask_clad(x,y)
+        e_field_clad = fclad(u, r, phi)*mclad
+
+        mode_field = e_field_core + e_field_clad
         return mode_field
--- tests/test_mtool.py
+++ tests/test_mtool.py
@@ -1,9 +1,26 @@
 from warnings import warn
 from mode_analysis.mtool import LPModes
+import numpy as np
 
 
 def test_init():
     lps = LPModes(v=2.0, dia_core=10)
     assert lps.u_set["u01"] == lps.u_lm(0,1)
 
-    
+
+def test_field_eq():
+    v = 3; d = 10; 
+    lps = LPModes(v, d)
+    l = 1; m = 1
+    u = lps.u_lm(l,m)
+    fcore = lps.field_core_eq(l)
+    fclad = lps.field_clad_eq(l)
+    assert np.isclose(fcore(u, d/2, 0), fclad(u, d/2, 0))
+
+    v = 4; d = 10; 
+    lps = LPModes(v, d)
+    l = 2; m = 1
+    u = lps.u_lm(l,m)
+    fcore = lps.field_core_eq(l)
+    fclad = lps.field_clad_eq(l)
+    assert np.isclose(fcore(u, d/2, np.pi/2), fclad(u, d/2, np.pi/2))
